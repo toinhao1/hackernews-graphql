0cf185dbbe838b5ac0b15d5e3f99e78bcc2611da
toinhao1 <toinhao89@gmail.com>
2019-07-25 13:20:53 +0200

    Able to create a link

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import logo from '../../src/logo.svg';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 import '../styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <LinkList />
-    </div>
-  );
+  return <CreateLink />;
 }
 
 export default App;
